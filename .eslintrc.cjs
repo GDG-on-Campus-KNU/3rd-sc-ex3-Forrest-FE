e39ac73ccdb7f802bb83862289b0416d9fb88844
KimKyuHoi <k546kh@gmail.com>
2024-02-12 00:07:18 +0900

    Env: 개발 환경 설정 vite+js, 라이브러리 설치

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // 추가
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'], // 'prettier' 플러그인 제거
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        'prettier/prettier': 'error', // 추가
+      },
+    },
+  ],
+};
