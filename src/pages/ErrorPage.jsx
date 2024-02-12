ef10869813b64bcc513078877228664bfeb067b1
KimKyuHoi <k546kh@gmail.com>
2024-02-12 23:48:03 +0900

    Feat: 에러 페이지 구성

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,35 @@
+import { Link } from 'react-router-dom';
+
+import { Typography, Button, Grid } from '@mui/material';
+
 const ErrorPage = () => {
   return (
-    <>
-      <main>
-        <h1> An error occurred!</h1>
-        <p>Could not find this page!</p>
-      </main>
-    </>
+    <Grid
+      container
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      style={{ minHeight: '100vh', padding: '20px' }}
+    >
+      <Grid item>
+        <Typography variant='h1' align='center' gutterBottom>
+          Oops!
+        </Typography>
+        <Typography variant='h4' align='center' gutterBottom>
+          An error occurred. The page you are looking for cannot be found.
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button
+          variant='contained'
+          component={Link}
+          to='/'
+          style={{ marginTop: '20px' }}
+        >
+          Back to Home
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
